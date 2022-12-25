c0d6139b5a111b7493e732ec637d15c6ff7e7d97
kevin <k8188219@gmail.com>
2022-12-25 23:09:06 +0800

    refactor: change worker compress function

--- canvas.js
+++ canvas.js
@@ -10,7 +10,7 @@ function compressImage1(img, cb) {
 
   setTimeout(() => {
     ctx.drawImage(img, 0, 0, img.naturalWidth * HEIGHT / img.naturalHeight, HEIGHT);
-  }, 3000)
+  }, 1000)
 
   cb(canvas);
 }
@@ -25,11 +25,15 @@ function compressImage2(file, img, cb) {
   canvas.height = HEIGHT;
   canvas.naturalWidth = canvas.width;
   canvas.naturalHeight = canvas.height;
+  var { width, height } = canvas
 
-  assignWorker({ file }).then(result => {
-    var { image_bitmap } = result;
-    requestAnimationFrame(() => ctx.drawImage(image_bitmap, 0, 0))
-  })
+  setTimeout(() => {
+    assignWorker({ file, width, height }).then(result => {
+      var { image_bitmap } = result;
+      requestAnimationFrame(() => ctx.drawImage(image_bitmap, 0, 0))
+    })
+  }, 1000)
+
 
   cb(canvas);
 }
@@ -40,18 +44,13 @@ function createWorker() {
   const workerCode = `
   self.onmessage = function(e) {
     const HEIGHT = 360;
-    var { file, id } = e.data;
-    var canvas = new OffscreenCanvas(100, 100);
-    var ctx = canvas.getContext('2d');
-
-    createImageBitmap(file).then(img => {
-      canvas.width = img.width * HEIGHT / img.height;
-      canvas.height = HEIGHT;
-      var { width, height } = canvas;
-      ctx.drawImage(img, 0, 0, width, height);
-      var image_bitmap = canvas.transferToImageBitmap();
-      self.postMessage({ id, image_bitmap });
-    })
+    var { file, width, height, id } = e.data;
+    // var canvas = new OffscreenCanvas(100, 100);
+    // var ctx = canvas.getContext('2d');
+    createImageBitmap(file, {resizeWidth:width, resizeHeight:height})
+      .then(image_bitmap => {
+        self.postMessage({ id, image_bitmap });
+      })
   };`;
   const blob = new Blob([workerCode], { type: 'text/javascript' });
   const url = URL.createObjectURL(blob);
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const TARGET_ROW_HEIGHT = 150;
-const IMAGE_BATCH_SIZE = 8;
+const TARGET_ROW_HEIGHT = 300;
+const IMAGE_BATCH_SIZE = 1000;
 const CREATE_IMAGE = "canvas";
 const COMPRESS_IMAGE = 2;
 const COMPRESS_WORKERS = 5;
